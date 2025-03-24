5190773a3955d36a3bafef267b0295f900d090a6
paulo <pauloramos.04@hotmail.com>
2025-03-24 11:11:05 -0300

    refactor: url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'plataforma-chamados-helps-production.up.railway.app',
+  baseURL: process.env.REACT_APP_API_URL || 'https://plataforma-chamados-helps-production.up.railway.app',
   headers: {
     'Content-Type': 'application/json',
   },
